7a6ff1c45c94d51d13c1db058f6e716a0ce3c2e5
ronmattss <ronmattss@gmail.com>
2024-01-02 21:47:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const port = 3000;
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello, this is the GET response!' });
 });
+app.get('test get', (req,res) =>
+{
+  res.json({ message: 'Get response test' });
+  
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+// Login system API
+// Create Account System API
+// Create a token that will be accepted for the current session
+// IOT dashboard 
